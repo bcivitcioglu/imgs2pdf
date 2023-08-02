27cd62c519d46b2ea59f2f8388aaed9253470c30
Burak Çivitcioglu <bcivitcioglu@ku.edu.tr>
2023-08-02 20:08:47 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,244 @@
+const express = require('express');
+const multer  = require('multer');
+const path = require('path');
+const fs = require('fs');
+const PDFDocument = require('pdfkit');
+const JSZip = require('jszip');
+const { PDFDocument: PDFLibDocument } = require('pdf-lib');
+const probe = require('probe-image-size');
+const heicConvert = require('heic-convert');
+
+const app = express();
+
+// Ensure uploads and pdfs directories exist
+const directories = ['uploads', 'pdfs'];
+directories.forEach(dir => {
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
+});
+// Unique Delimiter
+const delimiter = "____12345678____";
+
+// Multer setup
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        // Adjust the filename to include the delimiter and original filename
+        const originalFilename = path.basename(file.originalname, path.extname(file.originalname));
+        const filename = originalFilename + delimiter + Date.now() + path.extname(file.originalname);
+        cb(null, filename);
+    }
+});
+
+const upload = multer({ storage: storage });
+ 
+
+// Express setup
+app.use(express.static('public'));
+
+// Conversion progress storage
+let conversionProgress = {};
+
+// Deletion delay: 10 minutes
+const deletionDelay = 2 * 60 * 1000;
+
+function scheduleFileDeletion(filePath, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.error(`Failed to delete file at ${filePath}:`, err);
+                    reject(err); // Reject the promise if an error occurs
+                } else {
+                    console.log(`Successfully deleted file at ${filePath}`);
+                    resolve(); // Resolve the promise when the file is successfully deleted
+                }
+            });
+        }, delay);
+    });
+}
+
+// Route to handle file uploading
+app.post('/upload', upload.array('images', 30), async (req, res) => {
+    const mergePDFs = req.body.merge === 'true';
+
+    conversionProgress = {
+        total: req.files.length,
+        converted: 0
+    };
+
+    const convertImageToPdfPage = async (doc, imagePath, isFirstImage) => {
+        console.log(`Converting image at ${imagePath}...`);
+        // If the image is a HEIC image, convert it to JPEG first
+        let isHeic = path.extname(imagePath).toLowerCase() === '.heic';
+
+        if (isHeic) {
+            let jpegPath = imagePath + '.jpeg';
+            console.log(`Detected HEIC image. Converting to JPEG at ${jpegPath}...`);
+        
+            const inputBuffer = fs.readFileSync(imagePath);
+            const outputBuffer = await heicConvert({
+                buffer: inputBuffer, // the HEIC file buffer
+                format: 'JPEG',      // output format
+                quality: 1           // the jpeg compression quality, between 0 and 1
+            });
+        
+            fs.writeFileSync(jpegPath, outputBuffer);
+            imagePath = jpegPath;
+            // Schedule deletion of the converted JPEG file
+            scheduleFileDeletion(jpegPath, deletionDelay);
+
+        }
+        console.log(`Probing image at ${imagePath}...`);
+        const imageSize = await probe(fs.createReadStream(imagePath));
+
+    
+        const pdfWidth = doc.page.width - doc.page.margins.left - doc.page.margins.right;
+        const pdfHeight = doc.page.height - doc.page.margins.top - doc.page.margins.bottom;
+        const imageWidth = imageSize.width;
+        const imageHeight = imageSize.height;
+    
+        // Keep the image size the same if it fits within the page. Otherwise, scale it down.
+        let width, height;
+        if (imageWidth <= pdfWidth && imageHeight <= pdfHeight) {
+            // If the image fits within the page, keep its size the same.
+            width = imageWidth;
+            height = imageHeight;
+        } else {
+            // If the image is too big, scale it down.
+            const widthRatio = pdfWidth / imageWidth;
+            const heightRatio = pdfHeight / imageHeight;
+            const ratio = Math.min(widthRatio, heightRatio);
+    
+            width = imageWidth * ratio;
+            height = imageHeight * ratio;
+        }
+    
+        // Position the image in the center of the page
+        const x = (pdfWidth - width) / 2 + doc.page.margins.left;
+        const y = (pdfHeight - height) / 2 + doc.page.margins.top;
+    
+        if (!isFirstImage) {
+            doc.addPage();
+        }
+    
+        doc.image(imagePath, x, y, {width: width, height: height});
+    };
+    
+
+    if (mergePDFs) {
+        const doc = new PDFDocument();
+        const uniqueFilename = 'merged' + delimiter + Date.now() + '.pdf';
+        const stream = fs.createWriteStream(path.join('pdfs', uniqueFilename));
+
+        for (let i = 0; i < req.files.length; i++) {
+            let file = req.files[i];
+            await convertImageToPdfPage(doc, file.path, i === 0);
+            conversionProgress.converted += 1;
+            scheduleFileDeletion(file.path, deletionDelay); // Schedule deletion of uploaded file
+        }
+
+        doc.pipe(stream);
+        doc.end();
+
+    // Schedule deletion of generated PDF
+    scheduleFileDeletion(path.join('pdfs', uniqueFilename), deletionDelay); 
+
+    res.send({ pdfs: [uniqueFilename] });
+    } else {
+        const pdfs = [];
+        for (let i = 0; i < req.files.length; i++) {
+            let file = req.files[i];
+            const doc = new PDFDocument();
+            const stream = fs.createWriteStream(path.join('pdfs', file.filename + '.pdf'));
+            
+            await convertImageToPdfPage(doc, file.path, true);
+
+            doc.pipe(stream);
+            doc.end();
+
+            pdfs.push(path.join('pdfs', file.filename + '.pdf'));
+
+            conversionProgress.converted += 1;
+
+            // Schedule deletion of uploaded file and generated PDF
+            scheduleFileDeletion(file.path, deletionDelay);
+            scheduleFileDeletion(path.join('pdfs', file.filename + '.pdf'), deletionDelay);
+        }
+
+        res.send({ pdfs: pdfs.map(pdf => path.basename(pdf)) });
+    }
+});
+
+app.get('/download', async (req, res) => {
+    try{
+    if (!req.query.pdfs) {
+        res.status(400).send('Missing required parameter: pdfs');
+        return;
+    }
+
+    const pdfs = req.query.pdfs.split(',');
+    const zip = new JSZip();
+
+    if (pdfs.length === 1) {
+        const file = path.join('pdfs', pdfs[0]);
+
+        // Extract the original file name without the timestamp and image extension
+        const originalFilename = pdfs[0].split(delimiter)[0];
+        const pdfExtension = ".pdf";
+
+        // Split the original filename by the "." character
+        const filenameParts = originalFilename.split(".");
+
+        // If the filename has more than one part, remove the last part (image extension)
+        if (filenameParts.length > 1) {
+            filenameParts.pop();
+        }
+
+        // Join the remaining parts back together with the "." character
+        const filenameWithoutImageExtension = filenameParts.join(".");
+
+        res.set('Content-Disposition', `attachment; filename=${filenameWithoutImageExtension}${pdfExtension}`);
+        console.log(filenameWithoutImageExtension + pdfExtension);
+        const downloadFileName = filenameWithoutImageExtension + pdfExtension;
+        res.download(file,downloadFileName);
+
+    } else {
+        for (let pdf of pdfs) {
+            const data = fs.readFileSync(path.join('pdfs', pdf));
+            zip.file(pdf, data);
+        }
+
+        const data = await zip.generateAsync({type:'nodebuffer'});
+        res.set('Content-Type', 'application/zip');
+        res.set('Content-Disposition', `attachment; filename=pdfs.zip`);
+        res.send(data);
+    }}catch(err){
+        next(err);
+
+    }
+});
+
+app.get('/progress', (req, res) => {
+    res.json(conversionProgress);
+});
+
+app.get('/check-file', (req, res) => {
+    const { filePath } = req.query;
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        // fs.access checks for the existence of file
+        res.json({ fileExists: !err });
+    });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({ error: 'Something went wrong!' });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ public/js/script.js
@@ -0,0 +1,281 @@
+console.log("script.js is loaded");
+
+// Select the file input, dropzone, checkbox and buttons
+const fileInput = document.getElementById('fileInput');
+const dropzone = document.getElementById('dropzone');
+const mergeInput = document.getElementById('mergeInput');
+const convertButton = document.getElementById('convertButton');
+const downloadButton = document.getElementById('downloadButton');
+const statusText = document.getElementById('status');
+const removeAllButton = document.getElementById('removeAllButton');
+const dropText = document.getElementById('dropText');
+window.addEventListener('resize', adjustFontSize);
+
+function adjustFontSize() {
+    let fontSize = Math.max(window.innerWidth * 0.012, 16) + 'px';
+    document.body.style.fontSize = fontSize;
+
+    let buttons = document.querySelectorAll('.btn');
+    buttons.forEach(button => {
+        button.style.fontSize = fontSize;
+    });
+}
+
+// Call the function initially to set the font size
+adjustFontSize();
+
+let fileCountProgress = 0;  // Changed the name from uploadProgress to fileCountProgress
+let conversionProgress = 0;
+const maxFiles = 30;  // Set this to the maximum number of files allowed
+let allFiles = [];  // This array will hold all the files selected so far
+
+// Event listeners for dropzone
+dropzone.addEventListener('click', function() {
+    if (allFiles.length === maxFiles) {
+        return;
+    }
+    fileInput.click();
+});
+
+dropzone.addEventListener('dragover', function(e) {
+    if (allFiles.length === maxFiles) {
+        return;
+    }
+    e.preventDefault();
+    this.style.backgroundColor = '#aaa';
+});
+
+dropzone.addEventListener('dragleave', function(e) {
+    this.style.backgroundColor = 'rgb(233, 233, 233)';
+});
+
+dropzone.addEventListener('drop', function(e) {
+    e.preventDefault();
+    this.style.backgroundColor = 'rgb(233, 233, 233)';
+    addFiles(e.dataTransfer.files);
+});
+
+// Event listener for fileInput
+fileInput.addEventListener('change', function() {
+    addFiles(this.files);
+});
+
+function addFiles(files) {
+    if (allFiles.length === maxFiles) {
+        return;
+    }
+    Array.from(files).forEach(file => {
+        if (allFiles.length < maxFiles) {
+            allFiles.push(file);
+
+            const thumbnailContainer = document.createElement('div');
+            thumbnailContainer.className = 'thumbnailContainer d-flex align-items-start position-relative col-6 col-md-4 col-lg-4 p-1'; 
+
+            const thumbnail = document.createElement('img');
+            thumbnail.className = 'thumbnail img-thumbnail w-100 h-100'; 
+            thumbnail.style.objectFit = 'cover'; 
+
+            if (file.type === 'image/heic') {
+                thumbnail.src = '/logos/favicon.png'; // Replace with the path to your placeholder image
+            } else {
+                thumbnail.src = URL.createObjectURL(file);
+            }
+    
+            const removeButton = document.createElement('button');
+            removeButton.className = 'removeButton btn btn-sm btn-danger position-absolute top-0 end-0'; 
+            removeButton.style.zIndex = '10'; 
+            removeButton.textContent = '\u2715'
+    
+
+          removeButton.addEventListener('click', function(event) {
+              event.stopPropagation(); // Stop the click event from bubbling up to the dropzone
+              const index = allFiles.indexOf(file);
+              if (index !== -1) {
+                  allFiles.splice(index, 1);
+                  thumbnailContainer.remove();
+                  fileCountProgress = allFiles.length; // Update fileCountProgress
+                  convertButton.disabled = allFiles.length === 0;
+                  // Disable the "Remove All" button if no files are left
+                  removeAllButton.disabled = allFiles.length === 0;
+                  downloadButton.disabled = allFiles.length === 0;
+                  if (allFiles.length===0){
+                    dropText.textContent = 'Click or drop files here';
+                  }
+                  updateFileCountProgressBar();
+                  conversionProgress = 0;
+                  updateConversionProgressBar();
+              }
+              
+              if(allFiles.length===0){
+                statusText.textContent = 'No files selected';
+              } else {
+                statusText.textContent = allFiles.length + '/' + maxFiles + ' files selected';
+              }
+              statusText.style.color = 'blue';
+              downloadButton.disabled = true;  // Disable the download button
+          });
+            
+          thumbnailContainer.appendChild(thumbnail);
+          thumbnailContainer.appendChild(removeButton);
+
+          const thumbnailsContainer = document.getElementById('thumbnails');
+          thumbnailsContainer.appendChild(thumbnailContainer);
+}    if (allFiles.length>=1){
+    dropText.textContent = '';
+}
+      });
+
+    // Clear the file input
+    fileInput.value = '';
+
+    // Update fileCountProgress and the convert button
+    fileCountProgress = allFiles.length;
+    convertButton.disabled = allFiles.length === 0;
+
+    // Enable the "Remove All" button if there are files in the allFiles array
+    removeAllButton.disabled = allFiles.length === 0;
+
+    updateFileCountProgressBar();
+    statusText.textContent = allFiles.length + '/' + maxFiles + ' files selected';
+    statusText.style.color = 'blue';
+}
+
+
+
+convertButton.addEventListener('click', function() {
+    const formData = new FormData();
+    for (let i = 0; i < allFiles.length; i++) {
+        formData.append('images', allFiles[i]);
+        // Update the status text for each file
+        statusText.textContent = 'Uploading file ' + (i + 1) + ' of ' + allFiles.length;
+        statusText.style.color = 'blue';
+        
+    }
+    formData.append('merge', mergeInput.checked ? 'true' : 'false');
+
+    statusText.textContent = 'Converting...';
+    statusText.style.color = 'orange';
+    
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/upload', true);
+    
+    // xhr.upload.onprogress = (event) => {
+    //     if (event.lengthComputable) {
+    //         conversionProgress = event.loaded / event.total * 100;
+    //         updateConversionProgressBar();
+    //     }
+    // };
+
+
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            // Start updating the conversion progress bar as soon as the upload is complete
+            getConversionProgress();
+    
+            const result = JSON.parse(xhr.responseText);
+            console.log(result);
+
+            downloadButton.onclick = function() {
+                if (downloadButton.disabled) {
+                    return;
+                }
+                // Check if the file exists before starting the download
+                fetch(`/check-file?filePath=${encodeURIComponent('pdfs/' + result.pdfs[0])}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.fileExists) {
+                            // If the file exists, start the download
+                            fetch('/download?pdfs=' + encodeURIComponent(result.pdfs.join(',')))
+                                .then(response => response.blob())
+                                .then(blob => {
+                                    const url = window.URL.createObjectURL(blob);
+                                    const a = document.createElement('a');
+                                    a.href = url;
+                                    a.download = result.pdfs.length > 1 ? 'pdfs.zip' : result.pdfs[0];
+                                    a.click();
+                                })
+                                .catch(error => {
+                                    console.error('Error in fetch:', error);
+                                });
+                        } else {
+                            // If the file doesn't exist, disable the download button and inform the user
+                            downloadButton.disabled = true;
+                            statusText.textContent = 'File no longer exists. Please convert your files again.';
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Error in fetch:', error);
+                    });
+            };
+            
+            downloadButton.disabled = false;
+            downloadButton.style.display = 'block';
+            statusText.textContent = 'Conversion complete! Ready to download.';
+            statusText.style.color = 'green';
+                    }
+    };
+        xhr.send(formData);
+});
+
+removeAllButton.addEventListener('click', function() {
+    allFiles = [];  // Clear the allFiles array
+    fileCountProgress = 0;  // Reset fileCountProgress
+    convertButton.disabled = true;  // Disable the convert button
+    removeAllButton.disabled = true;  // Disable the "Remove All" button
+    downloadButton.disabled = true;
+    conversionProgress = 0;
+    updateConversionProgressBar();
+    updateFileCountProgressBar();  // Update the file count progress bar
+
+    // Remove all thumbnail containers
+    const thumbnailContainers = document.querySelectorAll('.thumbnailContainer');
+    thumbnailContainers.forEach(container => {
+        container.remove();
+    });
+    statusText.textContent = 'No files selected';
+    statusText.style.color = 'blue';
+    dropText.textContent = 'Click or drop files here';
+});
+
+mergeInput.addEventListener('change', function() {
+    downloadButton.disabled = true;  // Disable the download button
+});
+
+
+const getConversionProgress = () => {
+    fetch('/progress')
+        .then(response => {
+            console.log('Raw response from /progress:', response);
+            return response.json();
+        })
+        .then(data => {
+            conversionProgress = data.converted / data.total * 100;
+            updateConversionProgressBar();
+
+            if (conversionProgress < 100) {
+                setTimeout(getConversionProgress, 1000);
+            }
+        })
+        .catch(error => {
+            console.error('Error in getConversionProgress fetch:', error);
+        });
+};
+
+function updateFileCountProgressBar() {
+    const progressBar = document.getElementById('fileCountProgressBar').style;
+    const progress = (fileCountProgress / maxFiles) * 100;
+
+    // Update the width of the progress bar
+    progressBar.width = progress + '%';
+
+    // Update the color from green to red based on the number of files uploaded
+    progressBar.backgroundColor = `rgb(${progress * 2.55}, ${(100 - progress) * 2.55}, 0)`;
+
+}
+
+
+const updateConversionProgressBar = () => {
+    const conversionProgressBar = document.getElementById('conversionProgressBar');
+    conversionProgressBar.style.width = `${conversionProgress}%`;
+};
+
